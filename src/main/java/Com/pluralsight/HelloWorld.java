b767d0fd0330428930c772fac914c1f395b1d218
mrawesomeac@gmail.com <mrawesomeac@gmail.com>
2023-10-04 11:39:03 -0400

    completed ShoppingList app

---
+++ src/main/java/Com/pluralsight/HelloWorld.java
@@ -0,0 +1,6 @@
+package Com.pluralsight;
+
+public class HelloWorld {public static void main(String[] args) {
+    System.out.println("Hello World!");
+}
+}
---
+++ src/main/java/Com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package Com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Tomato,Lettuce,Cabbage,Ginger,Fish,Curry Powder,Water,Soy Sauce, Bread, and Cheese");
+    }
+}
